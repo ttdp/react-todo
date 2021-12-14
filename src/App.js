7b486d354b220c02032e6f34eb2a0c43a766f976
Cool <cool@Cools-MacBook-Air.local>
2021-12-14 08:34:32 +0800

    add todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container">
+            <div className="row">
+                <div className="todolist">
+                    <h1>Todos</h1>
+                    <ul className="list-unstyled">
+                        <li className="ui-state-default">
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" value=""/>Take out the trash
+                                </label>
+                            </div>
+                        </li>
+                        <li className="ui-state-default">
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" value=""/>Buy bread
+                                </label>
+                            </div>
+                        </li>
+                        <li className="ui-state-default">
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" value=""/>Teach penguins to fly
+                                </label>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+	);
 }
 
 export default App;
